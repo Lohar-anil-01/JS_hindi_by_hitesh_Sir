849fc108107ee1ffc6e1a0d0f15e94b13ea7dbaa
Lohar-anil-01 <anilatsas01@gmail.com>
2025-10-13 10:21:49 +0000

    DataTypes in js

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,31 @@
+"use strict" // Treat all JS code as newer version
+
+// alert("hello"); // alert using in browser console not in node
+// alert(3 + 3); // we are using node.js, not browser 
+
+
+/*console.log(3 
+    +
+     3); */ // code readability should be high
+console.log("MasterJi");
+
+let name = "Hitesh" //String
+let age = 18 // Number
+let isLoggedIn = false //Boolean
+
+/* Premitive data types */
+
+// number => range 2^53
+//bigint
+// String => Write between ""
+// Boolean => only two values 1)true or 2)false
+// null => Standalone value
+// undefined => still not assigned value
+// symbol => unique
+
+/* Non-Premitive data types */ 
+// object
+
+console.log(typeof null); // type is object
+console.log(typeof undefined); // type is undefined
+
