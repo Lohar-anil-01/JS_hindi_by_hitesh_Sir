8fc953bdb79ec611be7dde61094955651b451cbc
Lohar-anil-01 <anilatsas01@gmail.com>
2025-11-03 10:45:14 +0000

    Learn about while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,26 @@
+/* While Loop */
+
+// let index = 0;
+// while (index <= 10) {
+//     console.log(`Value of index is: ${index}`);
+//     index = index + 2;
+// }
+
+
+/* While Loop on array*/
+// let myArray = ["Ironman", "Spiderman", "Hulk", "Thor", "Captain America"];
+
+// let arr = 0;
+// while (arr < myArray.length) {
+//     console.log(`Value is ${myArray[arr]}`);
+//     arr = arr + 1;
+// }
+
+/* Do While Loop */
+
+let score = 11;
+
+do {
+    console.log(`Score is ${score}`);
+    score++;
+} while (score <= 10);
